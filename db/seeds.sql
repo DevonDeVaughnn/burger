75e6a62113740688cc1634656b33a7342106d7ae
Darrian DeVaughnn <devondevaughnn@gmail.com>
2020-10-16 19:58:46 -0700

    this is as good as its going to get

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (name, devoured) VALUES ('Double Bacon Cheddar', true);
+INSERT INTO burgers (name, devoured) VALUES ('The Luther', true);
+INSERT INTO burgers (name, devoured) VALUES ('The Joe Bitein', true);
+INSERT INTO burgers (name, devoured);
